6f3b18e775b287447b1c6553a24d25daeeaed8d1
明月有色 <2206582181@qq.com>
2016-03-23 14:03:14 +0800

    one index.php file

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// 应用入口文件
+if(!is_file('./Data/install.lock')){
+	if( strpos($_SERVER['REQUEST_URI'], '.php/Install') ===false ){
+		header("Location: ".$_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME'].'/Install');exit;
+	}
+}
+
+// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+define('APP_DEBUG',True);
+
+// 定义应用目录
+define('APP_PATH','./Application/');
+define("RUNTIME_PATH", './Runtime/');
+// 引入ThinkPHP入口文件
+require '../ThinkPHP/ThinkPHP.php';
+
+// 亲^_^ 后面不需要任何代码了 就是如此简单
